51bebfa6ae50b8e353b8ba794cdc95e3c58424a4
jinho6225 <jinho6225@hotmail.com>
2020-10-12 10:53:46 -0700

    vanilla todo redux

--- vanilla-todo-redux/index.js
+++ vanilla-todo-redux/index.js
@@ -1,7 +1,18 @@
 import { createStore } from 'redux';
 
-const reducer = (state, action) => {
 
+const reducer = (state = [], action) => {
+    switch(action.type) {
+        case "ADD":
+            return
+
+        default:
+            return state
+    }
 }
+
 const store = createStore(reducer)
+
 console.log(store, 'store')
+console.log(store.getState(), 'getState')
+
