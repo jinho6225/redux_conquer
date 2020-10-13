b1fbd724d4cf783b93bcf39d6b2f6f77231092ba
jinho6225 <jinho6225@hotmail.com>
2020-10-13 02:46:20 -0700

    connect, mapStateToProps, mapDispatchToProps, ownProps

--- react-redux-todo/src/routes/Detail.js
+++ react-redux-todo/src/routes/Detail.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
-function Detail() {
-    return <h2>Detail</h2>;
+function Detail({todo}) {
+    return (
+        <>
+            <h2>{todo?.todo}</h2>
+            <h5>Created At: {todo && todo.id}</h5>
+        </>
+    );
 }
 
-export default Detail
+const mapStateToProps = (state, ownProps) => {
+    const { match: { params : { id }} } = ownProps
+    return {
+        todo: state.find(todo => todo.id === parseInt(id))
+    }
+}
+
+export default connect(mapStateToProps)(Detail)
